b96b09931afb0916b68d332e86632eb0c2d67830
Hazlenut <38819915+Hazlenut@users.noreply.github.com>
2019-04-06 15:17:16 -0400

    Update Account.java

--- Account.java
+++ Account.java
@@ -1,1 +1,13 @@
 
+public class Account  {
+  private int id;
+  private String first_name;
+  private String last_name;
+  private int balance;
+  public Account(int id, String first_name, String last_name, int balance) {
+    this.id = id;
+    this.first_name = first_name;
+    this.last_name = last_name;
+    this.balance = balance;
+  }
+}
