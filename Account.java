b5b8750b76092bdf9879ac97fc432010f7d0f780
Hazlenut <38819915+Hazlenut@users.noreply.github.com>
2019-04-06 16:54:00 -0400

    Update Account.java

--- Account.java
+++ Account.java
@@ -1,10 +1,29 @@
 
-public class Account  {
-  private Customer id;
-  private int balance;
-  public Account(Customer id, int balance) {
-    this.id = id;
-    this.balance = balance;
-  }
- 
+
+import java.util.*;
+import java.io.*;
+
+public class Test {
+
+	public static void main(String[] args) throws FileNotFoundException {
+		// TODO Auto-generated method stub
+		String textfile = "SampleData.txt";
+		BufferedReader reader = new BufferedReader(new FileReader(textfile));
+		String first = "a", last = "s";
+		double balance = 0;
+		int id = 0;
+		try {
+
+			id = Integer.parseInt(reader.readLine());
+			first = reader.readLine();
+			last = reader.readLine();
+			balance = Double.parseDouble(reader.readLine());
+		} catch (IOException e) {
+			e.printStackTrace();
+
+		}
+		Customer m = new Customer(id, first, last, balance);
+		System.out.println(m.getFirstName());
+		System.out.println(m.getLastName());
+	}
 }
