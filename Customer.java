b534819a5c56846434093f704f65ea58a1d5eaa2
Hazlenut <38819915+Hazlenut@users.noreply.github.com>
2019-04-06 15:21:10 -0400

    Create Customer.java

---
+++ Customer.java
@@ -0,0 +1,10 @@
+public class Customer {
+  private int id;
+  private String first_name;
+  private String last_name;
+  public Customer(int id, String first_name, String last_name) {
+    this.id = id;
+    this.first_name = first_name;
+    this.last_name = last_name;
+    }
+    }
