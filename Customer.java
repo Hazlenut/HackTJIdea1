e18070b96a303e83753fff60836f80ac369a1387
Hazlenut <38819915+Hazlenut@users.noreply.github.com>
2019-04-06 16:54:26 -0400

    Update Customer.java

--- Customer.java
+++ Customer.java
@@ -1,10 +1,36 @@
+
+
 public class Customer {
-  private int id;
-  private String first_name;
-  private String last_name;
-  public Customer(int id, String first_name, String last_name) {
-    this.id = id;
-    this.first_name = first_name;
-    this.last_name = last_name;
-    }
-    }
+
+	private int id;
+	private static String first_name;
+	private String last_name;
+	private double balance;
+
+	public Customer(int id, String first_name, String last_name, double balance) {
+		this.id = id;
+		this.first_name = first_name;
+		this.last_name = last_name;
+		this.balance = balance;
+	}
+
+	public String getFirstName() {
+		
+		return first_name;
+
+	}
+
+	public String getLastName() {
+		return last_name;
+
+	}
+
+	public double getBalance() {
+		return balance;
+
+	}
+
+	public int getId() {
+		return id;
+	}
+}
